cca10ac1a483cc0d9d19105d18a60ee8f5531801
nata bagus <natabagusarisandi68@gmail.com>
2022-12-25 10:51:23 +0700

    register fiture has been created

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
 const loginRouter = require("./login/login");
+const registerRouter = require("./register/register");
 router.use("/login", loginRouter);
+router.use("/register", registerRouter);
 module.exports = router;
---
+++ routers/register/register.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const {} = require("../../controllers/register");
+router.post("/");
+
+module.exports = router;
