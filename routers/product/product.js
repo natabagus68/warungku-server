1397aac73b2394772dd2a5b01bcc4235fdbfa8ab
nata bagus <natabagusarisandi68@gmail.com>
2022-12-25 17:34:42 +0700

    fiture authentication

--- routers/product/product.js
+++ routers/product/product.js
@@ -4,7 +4,7 @@ const {
   get_product_specify_controller,
 } = require("../../controllers/product");
 const { authentication } = require("../../middlewares/authentication");
-router.post("/:UserId", generate_product_controller);
-router.get("/:UserId", authentication, get_product_specify_controller);
+router.post("/:UserId", authentication, generate_product_controller);
+router.get("/:UserId", get_product_specify_controller);
 
 module.exports = router;
--- routers/profile/profile.js
+++ routers/profile/profile.js
@@ -3,7 +3,8 @@ const {
   profile_controller,
   generate_profile_controller,
 } = require("../../controllers/profile");
+const { authentication } = require("../../middlewares/authentication");
 
 router.get("/:UserId", profile_controller);
-router.post("/:UserId", generate_profile_controller);
+router.post("/:UserId", authentication, generate_profile_controller);
 module.exports = router;
